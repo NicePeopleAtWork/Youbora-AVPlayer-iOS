0128f58b7890fd407b8ff85676bdb20d05559934
Joan Treveset <jtreveset@nicepeopleatwork.com>
2016-10-18 12:25:15 +0200

    Files for 5.3.4

---
+++ tvos/dynamic/YouboraPluginAVPlayerTvOS.framework/Headers/YouboraPluginAVPlayerTvOS.h
@@ -0,0 +1,20 @@
+//
+//  YouboraPluginAVPlayerTvOS.h
+//  YouboraPluginAVPlayerTvOS
+//
+//  Created by Joan on 01/07/16.
+//  Copyright © 2016 Nice People At Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouboraPluginAVPlayerTvOS.
+FOUNDATION_EXPORT double YouboraPluginAVPlayerTvOSVersionNumber;
+
+//! Project version string for YouboraPluginAVPlayerTvOS.
+FOUNDATION_EXPORT const unsigned char YouboraPluginAVPlayerTvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouboraPluginAVPlayerTvOS/PublicHeader.h>
+
+
+#import <YouboraPluginAVPlayerTvOS/YBPluginAVPlayer.h>
